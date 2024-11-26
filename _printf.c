16c55861398b6eb1dc5ee48c7a0eedc8fc489c20
JaysonPasquier <jaysonpasquier.contact@gmail.com>
2024-11-26 15:49:51 +0100

    modified the _printf.c files

--- _printf.c
+++ _printf.c
@@ -3,10 +3,35 @@
 #include "main.h"
 
 /**
- * print_number - Convertit un entier en base 10 et l'écrit.
- * @n: L'entier à convertir.
- *
- * Return: Le nombre de caractères écrits.
+ * print_char - print a charactere
+ * @c: the char to print
+ * Return: number of char printed (always 1)
+ */
+int print_char(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * print_string - print a string of char
+ * @s: the string to print
+ * Return: number of char printed
+ */
+int print_string(char *s)
+{
+	int count = 0;
+
+	if (!s)
+		s = "(null)";
+	while (*s)
+		count += print_char(*s++);
+	return (count);
+}
+
+/**
+ * print_number - convert a int to base 10 and print it
+ * @n: int to convert
+ * Return: number of char printed
  */
 int print_number(int n)
 {
@@ -15,23 +40,72 @@ int print_number(int n)
 	unsigned int num = (n < 0) ? -n : n;
 
 	if (n < 0)
-		count += write(1, "-", 1);
+		count += print_char('-');
 	do {
 		buffer[i++] = (num % 10) + '0';
 		num /= 10;
 	} while (num > 0);
 
 	while (i--)
-		count += write(1, &buffer[i], 1);
+		count += print_char(buffer[i]);
 
 	return (count);
 }
 
 /**
- * _printf - Fonction principale.
- * @format: Chaîne de format.
- *
- * Return: Nombre de caractères imprimés.
+ * handle_format - handle the spef of format
+ * @format: spef of the format
+ * @args: liste of variadique arguments
+ * Return: number of char printed for the spef
+ */
+int handle_format(const char **format, va_list args)
+{
+	int count = 0, i, base;
+	char buffer[32], *digits = "0123456789abcdef";
+	unsigned long int num;
+
+	if (**format == 'c')
+		return (print_char(va_arg(args, int)));
+	if (**format == 's')
+		return (print_string(va_arg(args, char *)));
+	if (**format == '%')
+		return (print_char('%'));
+	if (**format == 'd' || **format == 'i')
+		return (print_number(va_arg(args, int)));
+
+	if (**format == 'u' || **format == 'o' || **format == 'x' ||
+	 **format == 'X' || **format == 'p')
+	{
+		num = (**format == 'p') ? (unsigned long int)va_arg(args, void *) :
+			  va_arg(args, unsigned int);
+		base = (**format == 'o') ? 8 : (**format == 'u') ? 10 : 16;
+
+		if (**format == 'p')
+			count += print_string("0x");
+		if (**format == 'X')
+			digits = "0123456789ABCDEF";
+
+		i = 0;
+		do {
+			buffer[i++] = digits[num % base];
+			num /= base;
+		} while (num && i < 31);
+
+		while (i--)
+			count += print_char(buffer[i]);
+
+		return (count);
+	}
+
+	count += print_char('%');
+	count += print_char(**format);
+	return (count);
+}
+
+/**
+* _printf - Produces a formatted output according to a given string.
+ * @format: Format string.
+ * Return: The number of characters printed or -1 in case of an error.
  */
 int _printf(const char *format, ...)
 {
@@ -40,36 +114,17 @@ int _printf(const char *format, ...)
 
 	if (!format)
 		return (-1);
+
 	va_start(args, format);
 	while (*format)
 	{
-		if (*format == '%' && *(++format))
-		{
-			if (*format == 'c')
-			{
-
-				char c = va_arg(args, int);
-
-				count += write(1, &c, 1);
-			}
-			else if (*format == 's')
-			{
-				char *str = va_arg(args, char *);
-
-				if (!str)
-					str = "(null)";
-				while (*str)
-					count += write(1, str++, 1);
-			}
-			else if (*format == '%')
-				count += write(1, "%", 1);
-			else if (*format == 'd' || *format == 'i')
-				count += print_number(va_arg(args, int));
-		}
-		else
-			count += write(1, format, 1);
+		if (*format != '%')
+			count += print_char(*format);
+		else if (*(++format))
+			count += handle_format(&format, args);
 		format++;
 	}
 	va_end(args);
+
 	return (count);
 }
